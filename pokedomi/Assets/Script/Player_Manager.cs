c38483c2d4d94af00f7d86a8d8b89612ce1a62f6
yugiry <h1202419001@hiratagakuen.onmicrosoft.com>
2026-04-25 22:01:42 +0900

    レイキャストでクリックした位置からワールド座標を求める

--- pokedomi/Assets/Script/Player_Manager.cs
+++ pokedomi/Assets/Script/Player_Manager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Player_Manager : MonoBehaviour
 {
+    public Vector3 clickPos;
+
+    [SerializeField] Camera mainCam;//メインカメラ
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,22 @@ public class Player_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void ClickScreen()
+    {
+        //マウスのスクリーン座標をワールド座標に変換するためのRayを作成
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        //RaycastHitを使用して、クリックした位置のワールド座標を取得
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            //Rayがヒットしたオブジェクトのタグが"Ground"の場合のみ、クリックした位置を保存する
+            if (hit.collider.gameObject.tag == "Untagged")
+            {
+                //Rayがヒットした位置をクリックした位置として保存
+                clickPos = hit.point;
+            }
+        }
     }
 }
